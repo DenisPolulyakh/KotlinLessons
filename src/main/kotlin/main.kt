f535f26bdfa07c674139ff1cd10278a6c1b42116
Denis Polulyakh <denis.polulyakh@gmail.com>
2022-01-11 19:58:41 +0400

    объявление переменных  в kotlin

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var a: Int = 5
+    var b: Int = 3
+    var s: Int = a * b
+
+    var name: String = "rectangle"
+    var diagonal: Double = 5.83
+    var isSquare: Boolean = false
+
+    val f: Int = 10 // Константа
+
+    println(s)
+
+
+}
